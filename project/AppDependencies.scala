32273f85e78d16b305b3df18fa36ca74315d2702
Paul Blake <paul.blake@digital.hmrc.gov.uk>
2018-09-06 16:00:28 +0100

    CDD-1572: updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,10 +5,10 @@ object AppDependencies {
   val hmrcTestVersion = "3.0.0"
   val scalaTestVersion = "3.0.5"
   val scalatestplusVersion = "2.0.1"
-  val mockitoVersion = "2.18.3"
+  val mockitoVersion = "2.21.0"
   val pegdownVersion = "1.6.0"
-  val wireMockVersion = "2.17.0"
-  val customsApiCommonVersion = "1.27.0"
+  val wireMockVersion = "2.18.0"
+  val customsApiCommonVersion = "1.28.0"
   val circuitBreakerVersion = "3.2.0"
   val testScope = "test,it"
 
@@ -28,7 +28,7 @@ object AppDependencies {
 
   val customsApiCommonTests = "uk.gov.hmrc" %% "customs-api-common" % customsApiCommonVersion % testScope classifier "tests"
 
-  val hmrcApi = "uk.gov.hmrc" %% "play-hmrc-api" % "2.1.0"
+  val hmrcApi = "uk.gov.hmrc" %% "play-hmrc-api" % "3.2.0"
 
   val circuitBreaker = "uk.gov.hmrc" %% "reactive-circuit-breaker" % circuitBreakerVersion
 }
