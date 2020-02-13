4e125d6b2ee215b385591bc81323de5d12a16efc
Paul Blake <32386617+paulblakehmrc@users.noreply.github.com>
2020-02-13 11:31:40 +0000

    CDD-2672: updated sbt & scala and improved generation of schema zips (#77)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,17 +3,14 @@ import sbt._
 object AppDependencies {
 
   val hmrcTestVersion = "3.9.0-play-26"
-  val scalaTestVersion = "3.0.8"
-  val scalatestplusVersion = "3.1.2"
-  val mockitoVersion = "3.0.0"
-  val wireMockVersion = "2.23.2"
-  val customsApiCommonVersion = "1.42.0"
-  val circuitBreakerVersion = "3.3.0"
+  val scalatestplusVersion = "3.1.3"
+  val mockitoVersion = "3.2.4"
+  val wireMockVersion = "2.26.0"
+  val customsApiCommonVersion = "1.46.0"
+  val circuitBreakerVersion = "3.5.0"
   val testScope = "test,it"
 
   val hmrcTest = "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % testScope
-
-  val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion % testScope
 
   val scalaTestPlusPlay = "org.scalatestplus.play" %% "scalatestplus-play" % scalatestplusVersion % testScope
 
