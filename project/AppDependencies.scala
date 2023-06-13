67ec541ec63b73d631a97531813526d00b49f2df
Edward Oscar Taylor <48755019+ed1286@users.noreply.github.com>
2023-06-13 15:27:26 +0100

    DCWL-803-aligned-code

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val scalatestplusMockito    = "org.scalatestplus"                         %% "scalatestplus-mockito"  % "1.0.0-M2"  % testScope
   val wireMock                = "com.github.tomakehurst"                    %  "wiremock-standalone"    % "2.27.2"    % testScope
   val mockito                 = "org.mockito"                               % "mockito-core"            % "5.3.1"     % testScope
-  val flexmark                 = "com.vladsch.flexmark"                       % "flexmark-all"             % "0.35.10"   % testScope
+  val flexmark                = "com.vladsch.flexmark"                      % "flexmark-all"            % "0.35.10"   % testScope
   val jackson                 = "com.fasterxml.jackson.module"              %%  "jackson-module-scala"  % "2.15.0"    % testScope
   val customsApiCommon        = "uk.gov.hmrc"                               %% "customs-api-common"     % "1.60.0"    withSources()
   val customsApiCommonTests   = "uk.gov.hmrc"                               %% "customs-api-common"     % "1.60.0"    % testScope classifier "tests"
